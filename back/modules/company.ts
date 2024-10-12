a5b3d392459d2173d9e8fb7462388324d09e8b82
Renato Vicente <revi66@hotmail.com>
2024-10-12 12:47:14 +0200

    feat: more example types

--- back/modules/company.ts
+++ back/modules/company.ts
@@ -1,6 +1,21 @@
-import type Elysia from "elysia";
+import Elysia from "elysia";
 
-const list = (app: Elysia) => app.get("", () => {
+// Return types:
+// company: {
+//   index: {
+//       get: {
+//           body: unknown;
+//           params: {};
+//           query: unknown;
+//           headers: unknown;
+//           response: {
+//               ...;
+//           };
+//       };
+//   };
+// };
+
+const list = (app: Elysia) => app.get('', () => {
   return {
     items: [{id: 1, name: "Elysia"}],
     total: 1
@@ -8,4 +23,40 @@ const list = (app: Elysia) => app.get("", () => {
 });
 
 export const company = (app: Elysia) =>
-  app.group('/company', (app) => app.use(list))
+  app.group("/company", (app) => app.use(list));
+
+export const company2 = (app: Elysia) =>
+  app.group("/company", () => new Elysia({ name: "company", strictPath: true }).get('', () => {
+    return {
+      items: [{id: 1, name: "Elysia"}],
+      total: 1
+    }
+  }));
+
+export const company3 = new Elysia({ prefix: '/company', strictPath: true }).get('/', () => {
+  return {
+    items: [{id: 1, name: "Elysia"}],
+    total: 1
+  }
+})
+
+// Return types:
+// company: {
+//   get: {
+//       body: unknown;
+//       params: {};
+//       query: unknown;
+//       headers: unknown;
+//       response: {
+//           ...;
+//       };
+//   };
+// };
+
+export const workingCompany = (app: Elysia) =>
+  app.group("/company", (app) => app.get('', () => {
+    return {
+      items: [{id: 1, name: "Elysia"}],
+      total: 1
+    }
+  }));
